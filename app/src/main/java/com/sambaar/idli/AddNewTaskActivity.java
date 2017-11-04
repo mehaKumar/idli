0a4b6ca332991056489ba5e04f0832629090624e
Meha Kumar <mehaonweb@gmail.com>
2017-11-04 18:51:06 -0400

    add new task page

--- app/src/main/java/com/sambaar/idli/AddNewTaskActivity.java
+++ app/src/main/java/com/sambaar/idli/AddNewTaskActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 
 public class AddNewTaskActivity extends AppCompatActivity {
 
@@ -13,17 +14,6 @@ public class AddNewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_task);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
